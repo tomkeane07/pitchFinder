3c9f031acb047e8e64c41e5d46e2a73a74575ade
tom keane <tomkeane07@gmail.com>
2019-11-20 18:33:46 +0000

    mapSearch added

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,27 +1,3 @@
 function toggleMap() {
   $('#map').toggleClass("hide");
 }
-
-function AddPitch(e){
-    e.preventDefault();
-    var coord = [this.refs.alng.value,this.refs.alat.value];
-    alert(this.refs.alng.value+" "+this.refs.alat.value)
-    var name = this.refs.name.value;
-    var phone = this.refs.phone.value;
-    var data = {
-        name: name,
-        phone: phone,
-        geometry: {
-            type:"point",
-            coordinates:coord
-        }
-    }
-    console.log(data);
-    
-    $.ajax({
-        type: "POST",
-        url: "/api/pitches",
-        data: JSON.stringify(data),
-        contentType:"application/json"
-    });
-}
